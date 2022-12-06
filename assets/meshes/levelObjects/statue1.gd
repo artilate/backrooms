e5f8fa74630d453f2a5cc29bde089f1ba2e20c3f
artilate <aleckrsek@gmail.com>
2022-12-06 00:05:14 -0800

    update

--- assets/meshes/levelObjects/statue1.gd
+++ assets/meshes/levelObjects/statue1.gd
@@ -1,6 +1,9 @@
 extends Spatial
 
 var inside = false
+onready var image = $interactIndicator
+onready var player = get_node("../../Player")
+onready var head = player.get_node("Head")
 
 func _on_Area_area_entered(area):
 	inside = true
@@ -12,3 +15,5 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("interact") and inside == true:
 		self.visible = false
 		get_tree().get_current_scene().get_node("objects/table1/statue1").visible = true
+	image.global_rotation.x = head.rotation.x
+	image.global_rotation.y = player.rotation.y
